eb77da2b2a7b960141c763e5e18fa0e04e40eb90
Mike D'Arcy <m.darcy.220@gmail.com>
2017-10-22 17:38:50 -0400

    Change default data directory

--- data_load.py
+++ data_load.py
@@ -5,7 +5,9 @@ import re
 import os
 import numpy as np
 
-def get_images(data_dir='../data/img/train'):
+default_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'largedata', 'train')
+
+def get_images(data_dir=default_data_dir):
 	input_img_filenames = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if re.match('.*\\.(png|jpg)',f)]
 	input_imgs_raw = [scipy.ndimage.io.imread(file)[50:-50,50:-50] for file in input_img_filenames[:8000]]
 	input_imgs_reshaped = np.array([np.ascontiguousarray(np.rollaxis(np.rollaxis(np.array(img, dtype=np.float32), 0, 3), 0, 3)) for img in input_imgs_raw])
